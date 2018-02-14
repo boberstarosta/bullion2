c99e20c5b4406137e79a7cb5cde81169fbacfdc4
boberstarosta <boberstarosta@gmail.com>
2018-02-14 11:34:01 +0100

    Added test_last_value_per_gram

--- bullion/tests.py
+++ bullion/tests.py
@@ -1,3 +1,4 @@
+import time
 from django.test import TestCase
 from .models import Metal, Price
 
@@ -15,3 +16,10 @@ class PriceModelTests(TestCase):
     def test_value_per_gram(self):
         price = Price(metal=self.gold, value=3110.0)
         self.assertEqual(price.value_per_gram, 100.0)
+
+    def test_last_value_per_gram(self):
+        for value in range(5000, -1000, -1000):
+            Price.objects.create(metal=self.gold, value=value)
+            time.sleep(0.1)
+
+        self.assertEqual(Price.last_value_per_gram(self.gold), 0)
