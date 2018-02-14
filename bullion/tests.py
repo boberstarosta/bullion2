035fad78f7218d3cc35bedc46c382ae58ac3f056
boberstarosta <boberstarosta@gmail.com>
2018-02-14 11:10:17 +0100

    Created test_value_per_gram

--- bullion/tests.py
+++ bullion/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Metal, Price
 
-# Create your tests here.
+
+class MetalModelTests(TestCase):
+    def setUp(self):
+        pass
+
+
+class PriceModelTests(TestCase):
+    def setUp(self):
+        self.gold = Metal.objects.create(stooq_symbol='XAUPLN', name='Gold', short='Au')
+        self.silver = Metal.objects.create(stooq_symbol='XAGPLN', name='Silver', short='Ag')
+
+    def test_value_per_gram(self):
+        price = Price(metal=self.gold, value=3110.0)
+        self.assertEqual(price, 100.0)
