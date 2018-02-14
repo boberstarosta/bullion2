8bda14f5258ea4dff21399eeb09ed8def989573d
boberstarosta <boberstarosta@gmail.com>
2018-02-14 13:12:27 +0100

    Changed name coin_new to coin_create

--- website/urls.py
+++ website/urls.py
@@ -6,7 +6,7 @@ from bullion import views
 urlpatterns = [
     path('', views.MetalListView.as_view(), name='index'),
     path('coins/', views.CoinListView.as_view(), name='coin_list'),
-    path('coins/new', views.CoinCreateView.as_view(), name='coin_new'),
+    path('coins/new', views.CoinCreateView.as_view(), name='coin_create'),
     path('coins/<int:pk>/', views.CoinDetailView.as_view(), name='coin_detail'),
     path('coins/<int:pk>/edit', views.CoinUpdateView.as_view(), name='coin_update'),
     path('coins/<int:pk>/delete', views.CoinDeleteView.as_view(), name='coin_delete'),
