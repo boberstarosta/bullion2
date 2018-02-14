b78c02286ea88d31a88bcdcefc72be423962b3ad
boberstarosta <boberstarosta@gmail.com>
2018-02-14 17:17:44 +0100

    Overridden ready()

--- bullion/apps.py
+++ bullion/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+import stooq
 
 
 class BullionConfig(AppConfig):
     name = 'bullion'
+
+    def ready(self):
+        print('BullionConfig.ready here!')
+        stooq.start_updating(5)
