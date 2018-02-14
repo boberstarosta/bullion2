4638a532d0271f66506464c8a72268b8f79d3f9b
boberstarosta <boberstarosta@gmail.com>
2018-02-14 19:23:48 +0100

    Ajax works!

--- bullion/views.py
+++ bullion/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, BaseUpdateView
-from . import forms, models
+from . import forms, models, stooq
 
 
 class MetalListView(ListView):
@@ -38,5 +38,13 @@ class CoinDeleteView(DeleteView):
 class UpdateMetalPricesView(CreateView):
     def post(self, request):
         print('UpdateMetalPrices POST received')
-        data = {'text': 'I am a JsonResponse from django'}
+        stooq.update_metal_prices()
+        metals = models.Metal.objects.all()
+        data = {}
+        for metal in metals:
+            last_price = metal.last_price
+            data[metal.stooq_symbol] = {
+                'pricePerOz': last_price.value_per_oz,
+                'pricePerGram': last_price.value_per_gram,
+            }
         return JsonResponse(data)
