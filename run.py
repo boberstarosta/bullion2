ad0fb32864be4c6fc912834f77d865ca2b88b100
boberstarosta <boberstarosta@gmail.com>
2018-02-16 12:14:12 +0100

    installed pystray

---
+++ run.py
@@ -0,0 +1,13 @@
+import os
+from django.core.management import call_command
+from django.core.wsgi import get_wsgi_application
+
+
+
+
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
+
+    application = get_wsgi_application()
+    call_command('runserver',  'localhost:8080')
