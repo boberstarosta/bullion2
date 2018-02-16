e46511a1be52ce77fdbada4698d4774cece8a605
boberstarosta <boberstarosta@gmail.com>
2018-02-16 11:39:39 +0100

    Changed settings to production

--- website/settings.py
+++ website/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yg#)d6dbcp!0%91u&5&nk$p_*they=eq#uv+s)=@=^it+b#zuk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
