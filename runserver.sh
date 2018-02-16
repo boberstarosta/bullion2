abd42c6f643735d5e2a9d41b06c4e0f53708b4b4
boberstarosta <boberstarosta@gmail.com>
2018-02-16 14:31:41 +0100

    Added .desktop

--- runserver.sh
+++ runserver.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+echo "Script executed from: ${PWD}"
 
-sudo apt-get -y install screen </dev/null
+cat <<EOF >./Bullion.desktop
+[Desktop Entry]
+Type=Application
+Exec=${PWD}/runserver.sh
+Icon=${PWD}/icon.png
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name=Bullion
+GenericName=bullion
+X-GNOME-FullName=Bullion
+Comment=Bullion shortcut
+EOF
 
-screen
-source ./venv/bin/activate
-./venv/bin/python3 manage.py runserver localhost:8080 --noreload
-screen -d
+#sudo apt-get -y install screen </dev/null
+
+#screen
+#source ./venv/bin/activate
+#./venv/bin/python3 manage.py runserver localhost:8080 --noreload
+#screen -d
